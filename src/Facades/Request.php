c7e0371cf44fece63783a34fa777eb078829db13
KingMaj0r <abdurrahman@barakat.email>
2024-08-08 19:17:32 +0700

    added a new static function to retrieve the users ip for the Request facade.

--- src/Facades/Request.php
+++ src/Facades/Request.php
@@ -12,6 +12,26 @@ namespace PulseFrame\Facades;
  */
 class Request extends \Symfony\Component\HttpFoundation\Request
 {
+  /**
+   * Retrieve the client's IP address from the current request.
+   *
+   * @return string|null The IP address of the client if available; null otherwise.
+   */
+  public static function ip()
+  {
+    if (isset($_SERVER['HTTP_CLIENT_IP']) || isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      $ip = $_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } elseif (isset($_SERVER['HTTP_X_FORWARDED']) || isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) {
+      $ip = $_SERVER['HTTP_X_FORWARDED'] ?? $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
+    } elseif (isset($_SERVER['HTTP_FORWARDED_FOR']) || isset($_SERVER['HTTP_FORWARDED'])) {
+      $ip = $_SERVER['HTTP_FORWARDED_FOR'] ?? $_SERVER['HTTP_FORWARDED'];
+    } else {
+      $ip = $_SERVER['REMOTE_ADDR'] ?? null;
+    }
+
+    return $ip;
+  }
+
   public static function Capture()
   {
     return new self($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
